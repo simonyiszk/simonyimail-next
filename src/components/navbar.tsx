fef9aacdffcb6d7440f9f254baebf07db48321d4
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-12-10 00:52:10 +0100

    feat: link improvements

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,14 @@ export function Navbar() {
   return (
     <nav className='flex justify-between px-10 py-5 items-center shadow-sm sticky top-0 bg-white'>
       <h1>Simonyimail</h1>
-      <Link href='/templates'>Sablonok</Link>
+      <div className='flex gap-3'>
+        <Link className='link' href='/'>
+          Főoldal
+        </Link>
+        <Link className='link' href='/templates'>
+          Sablonok
+        </Link>
+      </div>
       <UserDisplay />
     </nav>
   );
--- src/components/user-display.tsx
+++ src/components/user-display.tsx
@@ -8,7 +8,9 @@ export function UserDisplay() {
   if (!data?.user) return <Link href={`/api/auth/signin`}>Bejelentkezés</Link>;
   return (
     <div className='flex items-center gap-5'>
-      <Link href={`/api/auth/signout`}>Kijelentkezés</Link>
+      <Link className='link' href={`/api/auth/signout`}>
+        Kijelentkezés
+      </Link>
       <p>{data.user.name}</p>
       {data.user.image && (
         <img src={data.user.image} alt='' referrerPolicy='no-referrer' className='rounded-full h-10 w-10 shadow-md' />
