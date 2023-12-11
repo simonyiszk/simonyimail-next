bb495b4cd72568f6467467c3ade956ed10ed955d
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-12-11 23:48:16 +0100

    feat: metadata to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,12 @@ const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='hu'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <title>Simonyimail</title>
+        <meta name='description' content='E-mail sablon készítő és levélküldő munkafolyam webalkalmazás' />
+      </head>
       <SessionProvider session={null}>
         <body className={inter.className}>
           <Navbar />
